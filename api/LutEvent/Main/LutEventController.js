18b25c8b52a6b839a6294c37d527b85ab034be31
TQNg <ntq.quan0904@gmail.com>
2017-11-26 00:14:26 +0700

    Reset TTL

--- api/LutEvent/Main/LutEventController.js
+++ api/LutEvent/Main/LutEventController.js
@@ -41,7 +41,7 @@ exports.createEvent = function (req,res) {
         return utils.result(res,code.badRequest,msg.invalidDuration,null);
     }
     var newEvent = Event(body);
-    // newEvent.ttl = body.estimated_duration ? body.estimated_duration.toString() + 's' : '10m';
+    newEvent.ttl = body.estimated_duration ? body.estimated_duration.toString() + 's' : '10m';
     User.findOne({
         _id:body.userId
     },function (err,userExist) {
